8fcb8f4ccda30e3087efdf17f30a11542064607b
wictormagnusson <wictor.magnusson@outlook.com>
2022-10-30 20:07:50 +0100

    Inlämning

--- OOP2_Project_Quiz_Game_1_1/Program.cs
+++ OOP2_Project_Quiz_Game_1_1/Program.cs
@@ -10,8 +10,24 @@ gameplay.Play();
 gameplay.ChooseCategory();
 Factory factory = new Factory(gameplay.Category, database, 4);
 
-//DisplayQuestionlist displayQuestionList = new DisplayQuestionlist(factory.NewPolitics.QuestionList, keepscore);
-DisplayQuestionlist displayQuestionList = new DisplayQuestionlist(factory.NewGeography.QuestionList, keepscore);
-//DisplayQuestionlist displayQuestionList = new DisplayQuestionlist(factory.NewMusic.QuestionList, keepscore);
+// Tillfällig lösning i brist på generics i displaymetoden.
 
-gameplay.PrintScore(displayQuestionList.score);
+if (gameplay.Category == "Politics")
+{
+    DisplayQuestionlist displayQuestionList = new DisplayQuestionlist(factory.NewPolitics.QuestionList, keepscore);
+    gameplay.PrintScore(displayQuestionList.score);
+}
+
+if (gameplay.Category == "Geography")
+{
+    DisplayQuestionlist displayQuestionList = new DisplayQuestionlist(factory.NewGeography.QuestionList, keepscore);
+    gameplay.PrintScore(displayQuestionList.score);
+}
+
+if (gameplay.Category == "Music")
+{
+    DisplayQuestionlist displayQuestionList = new DisplayQuestionlist(factory.NewMusic.QuestionList, keepscore);
+    gameplay.PrintScore(displayQuestionList.score);
+}
+
+
